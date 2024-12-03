a388cdcc6398a0685e7393f0a535712d219abc56
sohseyedi <sohseyedi@gmail.com>
2024-12-03 21:13:26 +0330

    feat: Add main layout structure with Header component and dynamic routing

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from './components/ui/Header.vue';
+</script>
+
+<template>
+  <Header/>
+  <main class="max-w-7xl mx-auto lg:px-0 px-3 py-5">
+    <router-view />
+  </main>
+</template>
+

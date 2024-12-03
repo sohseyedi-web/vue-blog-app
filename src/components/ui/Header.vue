b9f3b17c39363d8f44cb6e6c255eee332eab7c5b
sohseyedi <sohseyedi@gmail.com>
2024-12-03 21:16:03 +0330

    feat: Add navigation bar with home and create blog links

---
+++ src/components/ui/Header.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AppBtn from '../base/AppBtn.vue';
+</script>
+
+<template>
+    <nav class="py-4 border-b-2 border-zinc-800">
+        <div class="flex items-center justify-between max-w-7xl lg:px-0 px-4 mx-auto">
+            <router-link to="/">
+                <h2 class="text-3xl font-semibold text-emerald-500">ویوبلاگ</h2>
+            </router-link>
+            <router-link to="/create">
+                <AppBtn className="bg-emerald-500 hover:bg-green-500 lg:w-[150px] w-[130px] h-[45px]" title="ایجاد بلاگ"/>
+            </router-link>
+        </div>
+    </nav>
+</template>

972ca72b987ad616187b9eef96af9874ed708b92
sohseyedi <sohseyedi@gmail.com>
2024-12-03 21:17:01 +0330

    feat:create reusable button component

---
+++ src/components/base/AppBtn.vue
@@ -0,0 +1,9 @@
+<template>
+    <button :class="cn('bg-emerald-500 transition-all duration-300 rounded-2xl text-lg font-semibold hover:bg-green-500 text-white',className)">{{title}}</button>
+</template>
+
+<script setup>
+import cn from '../../utils/cn';
+
+const {title,className} = defineProps(['title','className'])
+</script>

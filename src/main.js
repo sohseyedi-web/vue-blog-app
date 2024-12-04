b6dbe1024c845b0e7716327f68774ac4e0347d3e
sohseyedi <sohseyedi@gmail.com>
2024-12-04 21:18:45 +0330

    chore:use toast for project

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@ import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import { router } from "./routes";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+const options = {
+  rtl: true,
+  icon: true,
+  position: "top-center",
+};
 
 
 createApp(App)
   .use(router)
   .use(createPinia())
+  .use(Toast, options)
   .mount("#app");

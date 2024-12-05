f8f036bfed9f158053d62eaef2ed241927a96a94
sohseyedi <sohseyedi@gmail.com>
2024-12-05 11:27:49 +0330

    feat: Create reusable section component with title and slot

---
+++ src/components/base/AppSection.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="w-full my-5">
+        <h5 class="text-xl font-bold text-emerald-500">{{title}}</h5>
+        <slot/>
+    </section>
+</template>
+<script setup>
+    defineProps(['title'])
+</script>
+

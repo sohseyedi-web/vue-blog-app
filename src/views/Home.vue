ac5d793a8d377a4d6b9b59eedcd9f506c77bac9b
sohseyedi <sohseyedi@gmail.com>
2024-12-05 11:27:23 +0330

    chore:adding app section in component with dynamic title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,23 @@
 <template>
-    <section class="w-full space-y-4 my-5">
-        <h5 class="text-xl font-bold text-emerald-500">جدیدترین بلاگ ها</h5>
-        <SwiperCard/>
-        <hr class="border-zinc-800 border"/>
-    </section>
-    <section class="w-full my-5">
-        <h5 class="text-xl font-bold text-emerald-500">همه بلاگ ها</h5>
-        <div class="flex items-center flex-wrap lg:gap-6 gap-3 my-5 text-zinc-100">
-            <AppCard v-for="item in sortItems" :key="item.id" :item="item" class="lg:w-[32%] w-full lg:mx-0 mx-auto"/>
-        </div>
+    <div class="flex items-center justify-center h-[80vh] text-xl font-semibold text-emerald-600" v-if="getItems.length === 0">
+        هنوز بلاگی ایجاد نکردید :(
+    </div>
+    <section v-else>
+        <AppSection class="space-y-4" title="جدیدترین بلاگ ها">
+            <SwiperCard/>
+            <hr class="border-zinc-800 border"/>
+        </AppSection>
+        <AppSection title="همه بلاگ ها">
+            <div class="flex items-center flex-wrap lg:gap-6 gap-3 my-5 text-zinc-100">
+                <AppCard v-for="item in sortItems" :key="item.id" :item="item" class="lg:w-[32%] w-full lg:mx-0 mx-auto"/>
+            </div>
+        </AppSection>
     </section>
 </template>
 
 <script setup>
 import AppCard from '../components/base/AppCard.vue';
+import AppSection from '../components/base/AppSection.vue';
 import SwiperCard from '../components/home/SwiperCard.vue';
 import useFormStore from '../store/useStore';
 

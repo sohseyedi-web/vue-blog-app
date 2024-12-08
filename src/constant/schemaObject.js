12a416d5bc65dfc76e01e1b27e9f7729bc464601
sohseyedi <sohseyedi@gmail.com>
2024-12-08 10:21:51 +0330

    chore:constant error form

--- src/constant/schemaObject.js
+++ src/constant/schemaObject.js
@@ -1,12 +1,2 @@
-import { z } from "zod";
-const TEXT_ERROR = (val) => `وارد کردن ' ${val} ' ضروری است`
-const PERSIAN_ERROR = `متن وارد شده "فارسی' نمی‌باشد.`
-
-
-export const formSchema = z.object({
-    title: z.string().min(1, TEXT_ERROR('عنوان')),
-    author: z.string().min(1, TEXT_ERROR('نام نویسنده')).regex(/^[\u0600-\u06FF\s]+$/, { message: PERSIAN_ERROR }),
-    subject: z.string().min(1, TEXT_ERROR('موضوع')),
-    message: z.string().min(1, TEXT_ERROR('متن بلاگ'))
-})
-
+export const TEXT_ERROR = (val) => `وارد کردن ' ${val} ' ضروری است`
+export const PERSIAN_ERROR = `متن وارد شده "فارسی' نمی‌باشد.`
